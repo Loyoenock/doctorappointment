94186a651d264794002024aadb2bf21942cb3558
loyoenock <loyoenock@gmail.com>
2024-09-10 09:26:09 +0300

    Section - Top doctors added

---
+++ frontend/src/components/TopDoctors.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { doctors } from '../assets/assets'
+
+const TopDoctors = () => {
+  return (
+    <section className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx=10'>
+      <h1 className='text-3xl font-medium'>Top Docors to Book</h1>
+      <p className='sm:w-1/3 text-center text-sm'> simply browse and book the best doctors in your city</p>
+      <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
+        {
+          doctors.slice(0, 10).map((doctor, i) => (
+            <div key={i} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10] transition-all duration-500'>
+              <img src={doctor.image} className='bg-blue-50' />
+              <div className='p-4'>
+                <div className='flex items-center gap-2 text-sm text-center text-green-500'>
+                  <p className='w-2 h-2 bg-green-500 rounded-full'></p><p>Available</p>
+                </div>
+                <p className='text-gray-900 font-lg font-medium'>{doctor.name}</p>
+                <p className='text-gray-600 text-sm'>{doctor.speciality}</p>
+              </div>
+             
+            </div>
+          ))
+        }         
+      </div>
+      <button className='text-gray-600 bg-blue-50 px-12 py-3 rounded-full mt-10'>More</button>
+    </section>
+  )
+}
+
+export default TopDoctors
