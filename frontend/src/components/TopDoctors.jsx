a767f251ca3aa46ae5d6aa1fc03cb09e7a328445
loyoenock <loyoenock@gmail.com>
2024-09-10 09:33:02 +0300

    Navigation to individual doctor page

--- frontend/src/components/TopDoctors.jsx
+++ frontend/src/components/TopDoctors.jsx
@@ -1,7 +1,11 @@
 import React from 'react'
 import { doctors } from '../assets/assets'
+import { useNavigate } from 'react-router-dom'
 
 const TopDoctors = () => {
+
+  const navigate = useNavigate();
+
   return (
     <section className='flex flex-col items-center gap-4 my-16 text-gray-900 md:mx=10'>
       <h1 className='text-3xl font-medium'>Top Docors to Book</h1>
@@ -9,7 +13,7 @@ const TopDoctors = () => {
       <div className='w-full grid grid-cols-auto gap-4 pt-5 gap-y-6 px-3 sm:px-0'>
         {
           doctors.slice(0, 10).map((doctor, i) => (
-            <div key={i} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10] transition-all duration-500'>
+            <div onClick={() => navigate(`/appointment/${doctor._id}`)} key={i} className='border border-blue-200 rounded-xl overflow-hidden cursor-pointer hover:translate-y-[-10] transition-all duration-500'>
               <img src={doctor.image} className='bg-blue-50' />
               <div className='p-4'>
                 <div className='flex items-center gap-2 text-sm text-center text-green-500'>
