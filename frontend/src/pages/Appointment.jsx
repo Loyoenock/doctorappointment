8e5f55921756e96bd2d16ba54ea2612fc19e3cf9
loyoenock <loyoenock@gmail.com>
2024-09-17 09:38:28 +0300

    Booking button added

--- frontend/src/pages/Appointment.jsx
+++ frontend/src/pages/Appointment.jsx
@@ -131,6 +131,7 @@ const Appointment = () => {
             ))
           }
         </div>
+        <button className='bg-primary text-white text-sm font-light px-14 py-3 rounded-full mt-6'>Book an Appointment</button>
       </div>
     </section>
   );
